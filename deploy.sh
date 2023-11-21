7ee24b1a5af760827662b196c2358978b164b552
innocent-greats <greatsinnocent@gmail.com>
2023-11-21 08:27:36 +0200

    init

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cd /home/greatsworks/projects/kafdrop/docker-compose/kafka-kafdrop
+docker-compose up -d
+
+cd /home/greatsworks/projects/beetroot/backend/microservices/services/kyc-service
+npm run dev
+
